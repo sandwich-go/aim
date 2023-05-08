1579bae909acdc4443cdc33ca1ee3d5f066637bc
hui.wang <hui.wang@centurygame.com>
2023-05-08 14:47:45 +0800

    feat: trigger

---
+++ src/components/cells/CellTrigger.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-dialog
+        modal
+        width="80%"
+        :close-on-press-escape="true"
+        :append-to-body="true"
+        :destroy-on-close="true"
+        :visible.sync="dialogVisible">
+      <slot :name="cc.slot"
+            :cc="cc"
+            :field-schema="fieldSchema"
+            :data="dataRef"
+            :field-name="fieldName"
+      />
+    </el-dialog>
+    <template v-if="!!cc.trigger.isButton">
+      <el-button
+          :disabled="disabled"
+          size="mini"
+          v-bind="cc.trigger"
+          @click="()=>{dialogVisible = true}"
+      >
+        <template v-if="!cc.trigger.circle">{{ cc.trigger.label ? cc.trigger.label : "" }}</template>
+      </el-button>
+    </template>
+    <template v-else>
+      <el-link :type="cc.trigger.type || 'primary'" @click="()=>{dialogVisible = true}">
+        <cell-view-icon v-if="cc.trigger.icon" :cell-config="cc.trigger.icon"/>
+        <cell-view-label v-if="cc.trigger.label" :cell-config="cc.trigger.label" style="padding-left: 3px"/>
+      </el-link>
+    </template>
+  </div>
+</template>
+
+<script>
+import MixinCellEditorConfig from "@/components/cells/mixins/MixinCellEditorConfig.vue";
+import CellViewIcon from "@/components/cells/CellViewIcon.vue";
+import CellViewLabel from "@/components/cells/CellViewLabel.vue";
+
+const jsb = require("@sandwich-go/jsb")
+
+export default {
+  name: 'CellTrigger',
+  components: {CellViewLabel, CellViewIcon},
+  mixins: [MixinCellEditorConfig],
+  created() {
+    this.ccConfigMerge()
+    this.cc.trigger = jsb.objectAssignNX(this.cc.trigger, {
+      isButton: false,
+      icon: 'el-icon-edit',
+    })
+  },
+  data() {
+    return {
+      dialogVisible: false,
+    }
+  },
+}
+</script>
