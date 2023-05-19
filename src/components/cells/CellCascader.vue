2cfa76bd5e14d3ff6469873a7e1b89c825342d60
hui.wang <hui.wang@centurygame.com>
2023-05-19 11:13:29 +0800

    feat:CellCascader

---
+++ src/components/cells/CellCascader.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-cascader
+      v-bind="cc"
+      :options="getOptions()"
+      clearable
+      size="mini"
+      v-model="dataRef[fieldName]"
+  >
+    <template slot-scope="{ node, data }">
+      <span>{{ data.label }}</span>
+      <span v-if="!node.isLeaf"> ({{ data.children.length }}) </span>
+    </template>
+  </el-cascader>
+</template>
+
+<script>
+
+import MixinCellEditorConfig from "@/components/cells/mixins/MixinCellEditorConfig.vue";
+
+export default {
+  name: 'CellCascader',
+  mixins: [MixinCellEditorConfig],
+  created() {
+    this.ccConfigMerge()
+    this.calcWidthPxString("100%")
+    console.log(this.options())
+  },
+}
+</script>
