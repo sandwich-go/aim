3de283b7688939f21761c175ee6fcc67bc8cc655
hui.wang <hui.wang@centurygame.com>
2023-09-05 11:41:05 +0800

    feat: rm debug

--- src/testing/AimTable.vue
+++ src/testing/AimTable.vue
@@ -463,8 +463,8 @@ export default {
         {
           field: 'AuthInfoObject', name: 'AuthInfoObject',
           type: 'object',
-          squash:true,
-          squashDivider:{icon:'el-icon-mobile-phone'},
+          squash:false,
+          // squashDivider:{icon:'el-icon-mobile-phone'},
           tips: '对象支持',
           cellFormConfig: {
             table: {
