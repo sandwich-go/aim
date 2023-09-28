49218de103397e766ada56685a4151ccf922b68a
hui.wang <hui.wang@centurygame.com>
2023-09-28 18:45:34 +0800

    fix: using require

--- src/components/AimTable/options.js
+++ src/components/AimTable/options.js
@@ -2,7 +2,7 @@ const jsb = require("@sandwich-go/jsb")
 
 // aimTableOptionPromise 外部使用，当table的field.option是一个Promise，防止多次产生loader行为
 // eslint-disable-next-line no-unused-vars
-export function aimTableOptionPromise(target,optionFetcher,optionLoaderProcess){
+export function aimTableOptionPromise(target,optionFetcher,optionLoaderProcess,until=jsb.until){
     const optionLoadedTo = `options_${jsb.xid()}`
     const optionLoaded = `${optionLoadedTo}_loaded`
     target[optionLoaded] = false
@@ -18,7 +18,7 @@ export function aimTableOptionPromise(target,optionFetcher,optionLoaderProcess){
         if(force){
             return optionLoader()
         }
-        return jsb.until(()=>target[optionLoaded] === true).then(()=>{
+        return until(()=>target[optionLoaded] === true).then(()=>{
                 return target[optionLoadedTo]
             }
         )
