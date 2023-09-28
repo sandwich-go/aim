7f4d33ac9c5cb832df70e9d66992cc41519d84a7
hui.wang <hui.wang@centurygame.com>
2023-09-28 18:56:18 +0800

    fix: using require

--- src/components/cells/CellViewLabel.vue
+++ src/components/cells/CellViewLabel.vue
@@ -1,20 +1,15 @@
 <template>
   <span v-if="fieldValue">
-    <template v-if="!isPlainObject(fieldValue)">{{fieldValue}}</template>
-    <template v-else>
       <span v-if="fieldValue.html" v-html='fieldValue.html' :style="fieldValue.style"></span>
       <span v-else :style="fieldValue.style">{{ (fieldValue.label || fieldValue.title)|| fieldValueFormatted}}</span>
-    </template>
   </span>
 </template>
 
 <script>
 import MixinCellViewConfig from "@/components/cells/mixins/MixinCellViewConfig.vue";
-import {isPlainObject} from "@/components/utils/jsb";
 
 export default {
   name: 'CellViewLabel',
-  methods: {isPlainObject},
   mixins: [MixinCellViewConfig],
 }
 </script>
