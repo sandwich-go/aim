3032638e74d8589045fa2a1ee4397cdd49d0c6be
hui.wang <hui.wang@centurygame.com>
2023-12-20 22:05:54 +0800

    fix: form footer

--- src/components/cells/CellViewHTML.vue
+++ src/components/cells/CellViewHTML.vue
@@ -1,14 +1,23 @@
 <template>
-  <div v-if="fieldValue">
-    <span v-html="fieldValue"></span>
+  <div v-if="htmlContent">
+    <span v-html="htmlContent"></span>
   </div>
 </template>
 
 <script>
 import MixinCellViewConfig from "@/components/cells/mixins/MixinCellViewConfig.vue";
+import jsb from "@sandwich-go/jsb";
 
 export default {
   name: "CellViewHTML",
   mixins: [MixinCellViewConfig],
+  computed:{
+    htmlContent(){
+      if(jsb.isObjectOrMap(this.fieldValue)){
+        return jsb.pathGet(this.fieldValue,'content',this.fieldValue)
+      }
+      return this.fieldValue
+    },
+  },
 }
 </script>
