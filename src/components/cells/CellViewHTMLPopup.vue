7864c3cb4b81ebb3b2c2781a2c769a16e56d2a8b
hui.wang <hui.wang@centurygame.com>
2023-12-20 21:57:42 +0800

    fix: form footer

--- src/components/cells/CellViewHTMLPopup.vue
+++ src/components/cells/CellViewHTMLPopup.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-if="fieldValue">
-    <el-popover trigger="hover">
+    <span v-if="isPlain()" v-html="content()"></span>
+    <el-popover v-else trigger="hover">
       <span v-html="fieldValue"></span>
       <el-button slot="reference" size="mini"><i class="el-icon-document"/></el-button>
     </el-popover>
@@ -9,9 +10,23 @@
 
 <script>
 import MixinCellViewConfig from "@/components/cells/mixins/MixinCellViewConfig.vue";
-
+import jsb from "@sandwich-go/jsb";
 export default {
   name: "CellViewHTMLPopup",
   mixins: [MixinCellViewConfig],
+  methods:{
+    isPlain(){
+      if(jsb.isObjectOrMap(this.fieldValue)){
+        return jsb.pathGet(this.fieldValue,'plain',true)
+      }
+      return false
+    },
+    content(){
+      if(jsb.isObjectOrMap(this.fieldValue)){
+        return jsb.pathGet(this.fieldValue,'content',this.fieldValue)
+      }
+      return this.fieldValue
+    }
+  }
 }
 </script>
