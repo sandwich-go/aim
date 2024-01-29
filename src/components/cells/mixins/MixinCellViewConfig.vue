ffac4e3c92377c00705d7222c2685e01578ee13b
hui.wang <hui.wang@centurygame.com>
2024-01-29 12:28:19 +0800

    fix: fieldValue

--- src/components/cells/mixins/MixinCellViewConfig.vue
+++ src/components/cells/mixins/MixinCellViewConfig.vue
@@ -7,8 +7,18 @@ export default {
   name: 'MixinCellViewConfig',
   mixins: [MixinCellEditorConfig],
   computed: {
+    // fixme 这里的逻辑有点乱，将 cell 的配置与数据混在了一起，需要分离重构
     fieldValue() {
-      return (jsb.isEmpty(this.cellConfig) && jsb.isPlainObject(this.cellConfig)) ? this.fieldValueFormatted : this.cellConfig
+      if(jsb.isNull(this.cellConfig)) {
+        return this.fieldValueFormatted
+      }
+      if(this.cellConfig && this.cellConfig.formatter){
+        return this.fieldValueFormatted
+      }
+      if(jsb.isEmpty(this.cellConfig) && jsb.isPlainObject(this.cellConfig)){
+        return this.fieldValueFormatted
+      }
+      return this.cellConfig
     },
     fieldValueFormatted(){
       return this.formatter ? this.formatter({value:jsb.pathGet(this.data, this.fieldName),row:this.data}) : jsb.pathGet(this.data, this.fieldName)
