dcdca39e84d9322268c382ee09a6f49ef5739822
hui.wang <hui.wang@centurygame.com>
2024-02-07 17:30:32 +0800

    feat: aim-table-small-padding

--- src/components/AimTable/mixins/MixinBasicProperty.vue
+++ src/components/AimTable/mixins/MixinBasicProperty.vue
@@ -50,7 +50,7 @@ export default {
       autoWidth: false,
       border: true,
       stripe: true,
-      class:jsb.clone(jsb.ccPath('aimTableClass', ['aim-table-mini-padding'])),
+      class:jsb.clone(jsb.ccPath('aimTableClass', ['aim-table-small-padding'])),
       showHeader: true,
       highlightCurrentRow: true,
       headerCellStyle: {textAlign: 'center', padding: '0', color: headerColor, background: headerBackgroundColor},
