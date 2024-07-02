2151dff63ae7d3c5dc41f53ed3c60d90615801c8
hui.wang <hui.wang@centurygame.com>
2024-07-02 19:53:10 +0800

    feat: old value

--- src/components/AimTable/mixins/MixinEdit.vue
+++ src/components/AimTable/mixins/MixinEdit.vue
@@ -124,7 +124,7 @@ export default {
     showFormEditorForRow(row) {
       // form 表单编辑逻辑,拷贝当前编辑行
       this.rowInEditForm = jsb.clone(row)
-      this.rowInEditFormBackup = jsb.clone(this.rowInEditFormBackup)
+      this.rowInEditFormBackup = jsb.clone(this.rowInEditForm)
       // 使用form的watcher
       this.cleanRowWatcher()
       this.rowFormEditorVisible = true
