902c534b611e90094f2f3dd7144ecc6a3efa1647
hui.wang <hui.wang@centurygame.com>
2024-11-14 19:10:33 +0800

    fix: padding bottom

--- src/components/AimTable/mixins/MixinHeader.vue
+++ src/components/AimTable/mixins/MixinHeader.vue
@@ -13,7 +13,7 @@ export default {
     }
   },
   created() {
-    this.headerConfigRef = initToolbarConfig(this.headerConfigRef, {'display':'flex','align-items':'center'})
+    this.headerConfigRef = initToolbarConfig(this.headerConfigRef, {'display':'flex','align-items':'center','padding-bottom':'3px'})
   }
 }
 </script>
