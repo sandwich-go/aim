10c39a54be8834048d3719a391a533acd1da04b3
hui.wang <hui.wang@centurygame.com>
2025-02-18 21:12:00 +0800

    feat: alert waring when query total error

--- src/components/cells/CellPager.vue
+++ src/components/cells/CellPager.vue
@@ -1,5 +1,9 @@
 <template>
+  <el-alert v-if="totalAlert" type="warning" size="mini" :closable="false">
+    <span slot="title" v-html="dataRef['PagerTotal']"></span>
+  </el-alert>
   <el-pagination
+      v-else
       :background="cc.background"
       :layout="cc.layout"
       :total="dataRef['PagerTotal']"
@@ -14,10 +18,16 @@
 <script>
 
 import MixinCellEditorConfig from "@/components/cells/mixins/MixinCellEditorConfig.vue";
+import jsb from "@cg-devcenter/jsb";
 
 export default {
   name:'CellPager',
   mixins: [MixinCellEditorConfig],
+  computed: {
+    totalAlert(){
+      return jsb.isString(this.dataRef['PagerTotal'])
+    }
+  },
   created() {
     this.ccConfigMerge()
   },
