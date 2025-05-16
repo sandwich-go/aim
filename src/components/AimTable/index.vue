866b8b7e278344cc40440a8b2841edf78c669388
hui.wang <hui.wang@centurygame.com>
2025-05-16 15:32:45 +0800

    refactor: field custom

--- src/components/AimTable/index.vue
+++ src/components/AimTable/index.vue
@@ -28,10 +28,8 @@
                   style="z-index:1000"
                   trigger="click">
                 <el-checkbox-group v-model="filedShowChecked" @change="filedShowCheckedChanged">
-                  <template v-for="field in schema">
-                    <div :key="`${field.name}-${field.field}`">
-                      <el-checkbox :label="field.name" :key="`${field.name}-${field.field}`">{{field.name}}</el-checkbox>
-                    </div>
+                  <template v-for="field in schema" :key="`${field.name}-${field.field}`">
+                    <el-checkbox v-if="!field['hideCustom']" :label="field.name">{{field['nameCustom']||field.name }}</el-checkbox>
                   </template>
                 </el-checkbox-group>
                 <el-button
